b8400a456ce33ae9573a8088682fdf4ea09bff68
Neel-020 <maheshgami12977@gmail.com>
2025-07-07 19:06:29 +0530

    fix

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -19,6 +19,7 @@ import {
 } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
 import { toast } from 'sonner';
+
 import * as z from 'zod';
 import { ApiResponse } from '@/types/ApiResponse';
 import { messageSchema } from '@/schemas/messageSchema';
